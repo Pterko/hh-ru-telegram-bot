47c7cd9903d4f84c02214e9df5dd60ab5978b9f2
Alexey Kolchanov <pter96@gmail.com>
2017-06-27 02:45:27 +0300

    Async await comeback

--- controllers/admin.js
+++ controllers/admin.js
@@ -5,12 +5,12 @@ var User = require("./../botModules/models/user");
 var Message = require('./../botModules/models/message');
 
 
-module.exports.adminPanelIndexPage =  (req, res, next) => {
+module.exports.adminPanelIndexPage = async (req, res, next) => {
     //total users
-    // let total_users = await User.count({});
-    // let auth_ok = await User.count({token: {"$exists": true}})
-    // let enabled_resume_update = await User.count({ $where: "this.autoUpdatedResumes.length >= 1" })
-    // let enabled_views_alert = await User.count({ $where: "this.lastTimeViews.length >= 1" })
+    let total_users = await User.count({});
+    let auth_ok = await User.count({token: {"$exists": true}})
+    let enabled_resume_update = await User.count({ $where: "this.autoUpdatedResumes.length >= 1" })
+    let enabled_views_alert = await User.count({ $where: "this.lastTimeViews.length >= 1" })
 
 
     let data = {
